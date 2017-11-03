ef3a5ada1c431988e709696ffc13ea09bf4b0757
carreerit <rwn0517@gmail.com>
2017-11-03 06:41:53 +0530

    Update deploy-for-test.sh

--- deploy-for-test.sh
+++ deploy-for-test.sh
@@ -8,10 +8,10 @@ cp /var/lib/jenkins/workspace/sample-test1/target/student*.war /tomcat/test/weba
 
 sleep 20
 
-RESPONSE=$(curl -s -I http://localhost:8082/student | head -1  | awk '{print $2}')
+RESPONSE=$(curl -s -I http://localhost:8082/student/index.jsp | head -1  | awk '{print $2}')
 echo RESPONSE = $RESPONSE
 if [ "$RESPONSE" != 200 ]; then 
-  echo "Respinse from student APP is not OK"
+  echo "Response from student APP is not OK"
   exit 1
 fi
   
