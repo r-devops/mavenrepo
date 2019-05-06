658f6e42b7437e1a82c456053569038c07de23ae
r-devops <raghuk.vit@gmail.com>
2019-05-06 18:12:20 +0530

    Update StudentDAO.java

--- src/com/srk/dao/StudentDAO.java
+++ src/com/srk/dao/StudentDAO.java
@@ -21,6 +21,35 @@ public class StudentDAO {
         return con;  
     }*/
 	
+	
+		/*public static Connection getConnection(){  
+        Connection con=null;  
+        try{  
+            Class.forName("com.mysql.jdbc.Driver");  
+            con=DriverManager.getConnection("jdbc:mysql://localhost:3306/test","root","root");  
+        }catch(Exception e){System.out.println(e);}  
+        return con;  
+    }*/
+	
+	
+		/*public static Connection getConnection(){  
+        Connection con=null;  
+        try{  
+            Class.forName("com.mysql.jdbc.Driver");  
+            con=DriverManager.getConnection("jdbc:mysql://localhost:3306/test","root","root");  
+        }catch(Exception e){System.out.println(e);}  
+        return con;  
+    }*/
+	
+	
+		/*public static Connection getConnection(){  
+        Connection con=null;  
+        try{  
+            Class.forName("com.mysql.jdbc.Driver");  
+            con=DriverManager.getConnection("jdbc:mysql://localhost:3306/test","root","root");  
+        }catch(Exception e){System.out.println(e);}  
+        return con;  
+    }*/
 	public static Connection getConnection() throws Exception {
 		Connection conn=null;
 		Context initContext = new InitialContext();
